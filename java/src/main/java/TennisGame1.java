53a4b87235f42b61b179ae0ad0decc211c56fdef
Simon <simonrosenberg1@gmail.com>
2021-09-24 10:24:04 +0100

    Refactor: Extract draw method in tennis game 1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,48 +1,32 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
+    private int m_score1 = 0; //what is an m-score? Variables should be renamed
     private int m_score2 = 0;
-    private String player1Name;
+    private String player1Name; //should this be an enum or a class?
     private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
+        this.player1Name = player1Name; //make name a variable of the class
         this.player2Name = player2Name;
     }
 
-    public void wonPoint(String playerName) {
+    public void wonPoint(String playerName) { //if player is an object, can just say, player1.score++
         if (playerName == "player1")
             m_score1 += 1;
         else
             m_score2 += 1;
     }
 
-    public String getScore() {
+    public String getScore() { //pull out methods, ie. check for draw(), check for win()
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        int tempScore=0; //variable naming? what is a temp score?
+        if (m_score1==m_score2) {
+            return draw();
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = m_score1-m_score2; //rename this variable
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -50,7 +34,7 @@ public class TennisGame1 implements TennisGame {
         }
         else
         {
-            for (int i=1; i<3; i++)
+            for (int i=1; i<3; i++) //this is going through this twice, insted make it a function, assign score to player
             {
                 if (i==1) tempScore = m_score1;
                 else { score+="-"; tempScore = m_score2;}
@@ -71,6 +55,20 @@ public class TennisGame1 implements TennisGame {
                 }
             }
         }
-        return score;
+        return score; //put this return throughout the function to avoid a lot of the if statements
+    }
+
+    private String draw() {
+            switch (m_score1)
+            {
+                case 0: //could turn this into an array with index and score names
+                        return "Love-All";
+                case 1:
+                        return "Fifteen-All";
+                case 2:
+                        return "Thirty-All";
+                default:
+                        return "Deuce";
+            }
     }
 }
