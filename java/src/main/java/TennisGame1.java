76bce147c6beb4c6abb769f757e910251f1799cc
Simon <simonrosenberg1@gmail.com>
2021-09-24 10:29:13 +0100

    Refactor extract win method in tennis game 1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -25,13 +25,7 @@ public class TennisGame1 implements TennisGame {
             return draw();
         }
         else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2; //rename this variable
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
+            return win();
         else
         {
             for (int i=1; i<3; i++) //this is going through this twice, insted make it a function, assign score to player
@@ -70,5 +64,14 @@ public class TennisGame1 implements TennisGame {
                 default:
                         return "Deuce";
             }
+    }
+
+    private String win() {
+            int minusResult = m_score1-m_score2; //rename this variable
+
+            if (minusResult==1) return "Advantage player1";
+            else if (minusResult ==-1) return "Advantage player2";
+            else if (minusResult>=2) return "Win for player1";
+            else return "Win for player2";
     }
 }
